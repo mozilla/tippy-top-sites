1f6d17759eec40381274b440e749c737bb017ad7
Ricky Rosario <rickyrosario@gmail.com>
2017-11-21 21:11:56 -0500

    simple manifest viewer

---
+++ manifest-viewer/main.js
@@ -0,0 +1,31 @@
+class Site extends React.PureComponent {
+  render() {
+    return (
+      <div class="tile">
+        <img src={this.props.image_url} title={JSON.stringify(this.props.domains)} />
+      </div>
+    );
+  }
+}
+
+class Sites extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        {this.props.sites.map((site, index) => <Site {...site} />)}
+      </div>
+    );
+  }
+}
+
+
+var myRequest = new Request("https://activity-stream-icons.services.mozilla.com/v1/icons.json.br");
+
+fetch(myRequest)
+  .then(function(response) { return response.json(); })
+  .then(function(data) {
+    ReactDOM.render(
+      <Sites sites={data} />,
+      document.getElementById("root")
+    );
+  });
